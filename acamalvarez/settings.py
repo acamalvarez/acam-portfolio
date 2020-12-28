0f0f2e92ce17827daa2c553e8abe0069e945377f
Camilo Álvarez <acalvm@gmail.com>
2020-12-28 12:09:17 -0500

    changing DEBUG

--- acamalvarez/settings.py
+++ acamalvarez/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env.bool('DEBUG', default=False)
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'acam-portfolio.herokuapp.com']
 
